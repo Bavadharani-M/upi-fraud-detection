c6e16a8dfc8ab9fbccc390fd0e3a5d06ea7db554
M.Bavadharani <bavadharanim007@gmail.com>
2026-06-08 15:59:16 +0530

    fix css import

--- src/upiauthmock.js
+++ src/upiauthmock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./upiAuthMock.css";
+import "./upiauthmock.css";
 
 const UPIAuthMock = () => {
   const navigate = useNavigate();
